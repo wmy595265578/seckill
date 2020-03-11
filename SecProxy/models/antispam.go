a51ed7a6738cb1150b9dfddc98b47543411f6935
JustDoIt <595265578@qq.com>
2020-03-11 14:39:00 +0800

    20200311

--- SecProxy/models/antispam.go
+++ SecProxy/models/antispam.go
@@ -2,12 +2,13 @@ package models
 
 import (
 	"fmt"
+	"github.com/astaxie/beego/logs"
 	"sync"
 )
 
 var (
 	secLimitMgr = &SecLimitMgr{
-		UserLimitMap:make(map[int]*Limit,1000),
+		UserLimitMap: make(map[int]*Limit, 1000),
 	}
 	seclimit = &Limit{}
 )
@@ -19,16 +20,71 @@ type SecLimitMgr struct {
 }
 
 func antiSpam(req *SecRequest) (err error) {
-	secLimitMgr.Lock.Lock()
-	defer secLimitMgr.Lock.Unlock()
+	//secLimitMgr.Lock.Lock()
+	//defer secLimitMgr.Lock.Unlock()
+	//
+	//secLimit, ok := secLimitMgr.UserLimitMap[req.UserId]
+	//if !ok {
+	//	secLimitMgr.UserLimitMap[req.UserId] = secLimit
+	//}
+	//if  seclimit.secLimit.Count(req.AccessTime.Unix()) > SeckillConf.UserSecAccessLimit {
+	//	err = fmt.Errorf("invalid count ")
+	//	return
+	//}
 
-	secLimit, ok := secLimitMgr.UserLimitMap[req.UserId]
-	if !ok {
-		secLimitMgr.UserLimitMap[req.UserId] = secLimit
-	}
-	if  seclimit.secLimit.Count(req.AccessTime.Unix()) > SeckillConf.UserSecAccessLimit {
-		err = fmt.Errorf("invalid count ")
+	_, ok := SeckillConf.idBlackMap[req.UserId]
+	if ok {
+		err = fmt.Errorf("invalid request")
+		logs.Error("userId[%v] is block by id black", req.UserId)
 		return
 	}
+
+	_, ok = SeckillConf.ipBlackMap[req.ClientAddr]
+	if ok {
+		err = fmt.Errorf("invalid request")
+		logs.Error("userId[%v] ip[%v] is block  by ip black", req.UserId, req.ClientAddr)
+		return
+	}
+
+	SeckillConf.SecLimitMgrConf.Lock.Lock()
+	//user Id
+	limit, ok := SeckillConf.SecLimitMgrConf.UserLimitMap[req.UserId]
+	if !ok {
+		limit = &Limit{
+			secLimit: &SecLimit{},
+			minLimit: &MinLimit{},
+		}
+		SeckillConf.SecLimitMgrConf.UserLimitMap[req.UserId] = limit
+	}
+
+	secIdCount := limit.secLimit.Count(req.AccessTime.Unix())
+	minIdCount := limit.minLimit.Count(req.AccessTime.Unix())
+
+	//user Ip
+
+	limit, ok = SeckillConf.SecLimitMgrConf.IpLimitMap[req.ClientAddr]
+	secIpCount := limit.secLimit.Count(req.AccessTime.Unix())
+	minIpCount := limit.minLimit.Count(req.AccessTime.Unix())
+
+	if secIdCount > SeckillConf.AccessLimitConf.UserSecAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	if minIdCount > SeckillConf.AccessLimitConf.UserMinAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	if secIpCount > SeckillConf.AccessLimitConf.IPSecAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	if minIpCount > SeckillConf.AccessLimitConf.IPMinAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
 	return
 }
